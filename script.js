9517d8aafc514cd076387689b9313fa44b5ee25f
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-02-03 09:35:24 +0100

    new commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function describeCountry(country, population, capitalCity){
+    return("country has population people and its capital city is capitalCity")
+}
+describeCountry(lagos, fourmillion, ikeja)
